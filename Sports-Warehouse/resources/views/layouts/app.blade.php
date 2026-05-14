38428756e96ba9408c882a81fae038f202dbb71e
scollap <tom_scollay@live.com.au>
2026-05-14 19:36:12 +1000

    update to use vite

--- Sports-Warehouse/resources/views/layouts/app.blade.php
+++ Sports-Warehouse/resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <title>@yield('title', 'SW Warehouse')</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/modern-normalize@3.0.1/modern-normalize.min.css">
         <link rel="stylesheet" href="{{ asset('styles/style.css') }}">
+        @vite(['resources/css/app.css', 'resources/js/app.js']) <!-- to use css tailwind -->
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
     </head>
     <body>
