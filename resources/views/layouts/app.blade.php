0fade3d7819febc1f5a6a036749c3bab15dcc632
scollap <tom_scollay@live.com.au>
2026-06-11 20:57:39 +1000

    Scaffolded laravel breeze templates and routes and merged with our existing templates and routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>@yield('title', 'SW Warehouse')</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/modern-normalize@3.0.1/modern-normalize.min.css">
         {{-- <link rel="stylesheet" href="{{ asset('css/style.css') }}">  --}}
