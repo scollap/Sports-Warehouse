4e28dc31abb1f97ce3c5ae6ec20f7521e2a2e520
scollap <tom_scollay@live.com.au>
2026-06-17 20:36:29 +1000

    added some DIV's on checkout form so form is has a nicer format

--- resources/views/checkout/show.blade.php
+++ resources/views/checkout/show.blade.php
@@ -63,110 +63,112 @@
 
                 <fieldset>
                     <legend>Checkout Details</legend>
+                    <div class="flex gap-4">
+                        <div class="form-row">
+                            <label for="customer_firstname">First Name*</label>
 
-                    <div class="form-row">
-                        <label for="customer_firstname">First Name*</label>
+                            <input
+                                class="form-input"
+                                type="text"
+                                id="customer_firstname"
+                                name="customer_firstname"
+                                value="{{ old('customer_firstname') }}"
+                                required
+                            >
 
-                        <input
-                            class="form-input"
-                            type="text"
-                            id="customer_firstname"
-                            name="customer_firstname"
-                            value="{{ old('customer_firstname') }}"
-                            required
-                        >
+                            @error('customer_firstname')
+                                <span class="error-message">
+                                    {{ $message }}
+                                </span>
+                            @enderror
+                        </div>
 
-                        @error('customer_firstname')
-                            <span class="error-message">
-                                {{ $message }}
-                            </span>
-                        @enderror
+                        <div class="form-row">
+                            <label for="customer_lastname">Last Name*</label>
+                            <input
+                                class="form-input"
+                                type="text"
+                                id="customer_lastname"
+                                name="customer_lastname"
+                                value="{{ old('customer_lastname') }}"
+                                required
+                            >
+
+                            @error('customer_lastname')
+                                <span class="error-message">
+                                    {{ $message }}
+                                </span>
+                            @enderror
+                        </div>
                     </div>
+                    <div class="flex gap-4">
+                        <div class="form-row">
+                            <label for="customer_phone">Phone Number</label>
 
-                    <div class="form-row">
-                        <label for="customer_lastname">Last Name*</label>
-                        <input
-                            class="form-input"
-                            type="text"
-                            id="customer_lastname"
-                            name="customer_lastname"
-                            value="{{ old('customer_lastname') }}"
-                            required
-                        >
+                            <input
+                                class="form-input"
+                                type="text"
+                                id="customer_phone"
+                                name="customer_phone"
+                                value="{{ old('customer_phone') }}"
+                            >
+                            @error('customer_phone')
+                                <span class="error-message">
+                                    {{ $message }}
+                                </span>
+                            @enderror
+                        </div>
 
-                        @error('customer_lastname')
-                            <span class="error-message">
-                                {{ $message }}
-                            </span>
-                        @enderror
+                        <div class="form-row">
+                            <label for="customer_email">Your Email*</label>
+
+                            <input
+                                class="form-input"
+                                type="email"
+                                id="customer_email"
+                                name="customer_email"
+                                value="{{ old('customer_email') }}"
+                                required
+                            >
+
+                            @error('customer_email')
+                                <span class="error-message">
+                                    {{ $message }}
+                                </span>
+                            @enderror
+                        </div>
                     </div>
+                    <div class="flex gap-4">
+                        <div class="form-row">
+                            <label for="address_street">Street Address*</label>
+                            <input
+                                class="form-input"
+                                type="text"
+                                id="address_street"
+                                name="address_street"
+                                value="{{ old('address_street') }}"
+                                required
+                            >
+                            @error('address_street')
+                                <span class="error-message">{{ $message }}</span>
+                            @enderror
+                        </div>
 
-                    <div class="form-row">
-                        <label for="customer_phone">Phone Number</label>
-
-                        <input
-                            class="form-input"
-                            type="text"
-                            id="customer_phone"
-                            name="customer_phone"
-                            value="{{ old('customer_phone') }}"
-                        >
-                        @error('customer_phone')
-                            <span class="error-message">
-                                {{ $message }}
-                            </span>
-                        @enderror
-                    </div>
-
-                    <div class="form-row">
-                        <label for="customer_email">Your Email*</label>
-
-                        <input
-                            class="form-input"
-                            type="email"
-                            id="customer_email"
-                            name="customer_email"
-                            value="{{ old('customer_email') }}"
-                            required
-                        >
-
-                        @error('customer_email')
-                            <span class="error-message">
-                                {{ $message }}
-                            </span>
-                        @enderror
-                    </div>
-
-                    <div class="form-row">
-                        <label for="address_street">Street Address*</label>
-                        <input
-                            class="form-input"
-                            type="text"
-                            id="address_street"
-                            name="address_street"
-                            value="{{ old('address_street') }}"
-                            required
-                        >
-                        @error('address_street')
-                            <span class="error-message">{{ $message }}</span>
-                        @enderror
-                    </div>
-
-                    <div class="form-row">
-                        <label for="address_suburb">Suburb*</label>
-                        <input
-                            class="form-input"
-                            type="text"
-                            id="address_suburb"
-                            name="address_suburb"
-                            value="{{ old('address_suburb') }}"
-                            required
-                        >
-                        @error('address_suburb')
-                            <span class="error-message">{{ $message }}</span>
-                        @enderror
-                    </div>
-
+                        <div class="form-row">
+                            <label for="address_suburb">Suburb*</label>
+                            <input
+                                class="form-input"
+                                type="text"
+                                id="address_suburb"
+                                name="address_suburb"
+                                value="{{ old('address_suburb') }}"
+                                required
+                            >
+                            @error('address_suburb')
+                                <span class="error-message">{{ $message }}</span>
+                            @enderror
+                        </div>
+                   </div>
                     <div class="flex gap-4">
                         <div class="form-row w-1/2">
                             <label for="address_state">State*</label>
